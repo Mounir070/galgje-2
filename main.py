b0458af2f46a0a5f4c17db1bb2e2b002d3bac299
Mounir Kalai <57255735+Mounir070@users.noreply.github.com>
2021-07-07 22:32:59 +0000

    intro

---
+++ main.py
@@ -0,0 +1,10 @@
+# introductie
+  naam = input("Vul hier je naam in: ")
+  print()
+  print("Welkom", naam,"bij ons galgje spel")
+  game = True
+
+  print("Raad de letters en als je het woord weet typ het dan in.")
+  print()
+  print("het woord heeft " + str(lengtewoord) + " letters")
+  
