63fc72757fc3441beb198514993619ca9f5f672d
Mounir Kalai <57255735+Mounir070@users.noreply.github.com>
2021-07-07 22:33:41 +0000

    deel 1

--- main.py
+++ main.py
@@ -7,4 +7,14 @@
   print("Raad de letters en als je het woord weet typ het dan in.")
   print()
   print("het woord heeft " + str(lengtewoord) + " letters")
-  
+   #als het woord geraden is
+  while game == True:
+    userGuess = input("raad een letter of het woord: ")
+    if userGuess == hetwoord:
+      print ("gefeliciteerd", naam, "je hebt het woord geraden")
+      print()
+      print("wil je nog een keer spelen? Type dan ja of nee?")
+      print("Goed gedaan!!! je had alleen deze letters nodig om het woord te raden")
+    #speler kan kiezen om opnieuw te spelen
+    if userGuess == ja:
+       spel()
